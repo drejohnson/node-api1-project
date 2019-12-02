a76c8d6bca36f3560fc739524449c98528ef8826
DeAndre Johnson <dre@phreshr.com>
2019-12-03 04:39:21 +0800

    add post route

--- routes/users.mjs
+++ routes/users.mjs
@@ -15,4 +15,22 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.post("/", async (req, res) => {
+  const { name, bio } = req.body;
+  if (!name || !bio) {
+    return res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  }
+  try {
+    const newUser = await db.insert({ name, bio });
+    res.status(201).json(newUser);
+  } catch (error) {
+    console.log("The users information could not be retrieved.", error);
+    res
+      .status(500)
+      .json({ error: "The users information could not be retrieved." });
+  }
+});
+
 export default router;
