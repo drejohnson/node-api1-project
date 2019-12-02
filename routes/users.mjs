549e565178d01575216a280ad7cf1e0786062a97
DeAndre Johnson <dre@phreshr.com>
2019-12-03 04:48:21 +0800

    add GET route to get user by id

--- routes/users.mjs
+++ routes/users.mjs
@@ -15,13 +15,33 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await db.findById(id);
+
+    if (!user)
+      return res
+        .status(404)
+        .json({ message: "The user with the specified ID does not exist." });
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.log("The users information could not be retrieved.", error);
+    res
+      .status(500)
+      .json({ error: "The user information could not be retrieved." });
+  }
+});
+
 router.post("/", async (req, res) => {
   const { name, bio } = req.body;
-  if (!name || !bio) {
+  if (!name || !bio)
     return res
       .status(400)
       .json({ errorMessage: "Please provide name and bio for the user." });
-  }
+
   try {
     const newUser = await db.insert({ name, bio });
     res.status(201).json(newUser);
