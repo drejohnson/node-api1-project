b62cc69a0525e79828607a57d6d95cea95667d1e
DeAndre Johnson <dre@phreshr.com>
2019-12-03 04:09:42 +0800

    initial server set up

--- index.mjs
+++ index.mjs
@@ -1,1 +1,13 @@
-// implement your API here
+import express from "express";
+
+const port = process.env.PORT || 4000;
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send({ api: "API up and running" });
+});
+
+server.listen(port, () => console.log(`Server running on port: ${port}`));
