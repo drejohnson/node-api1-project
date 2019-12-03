88f8effaea56632c6a1bf2fc890ef2e4c10ca01b
DeAndre Johnson <dre@phreshr.com>
2019-12-03 11:29:32 +0800

    feature: edit user

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
+import EditUser from "./EditUser";
+
 const User = ({ user, users, setUsers }) => {
+  const [edit, setEdit] = useState(false);
+
+  const toggleEdit = () => setEdit(!edit);
+
   const deleteUser = async () => {
     const id = user && user.id;
     const newUsers = users && users.filter(user => user.id !== id);
@@ -13,16 +19,27 @@ const User = ({ user, users, setUsers }) => {
     }
   };
   return (
-    <div>
-      <div>
-        <h2>{user.name}</h2>
-        <p>{user.bio}</p>
-      </div>
-      <div>
-        <button>Edit</button>
-        <button onClick={deleteUser}>Delete</button>
-      </div>
-    </div>
+    <>
+      {edit ? (
+        <EditUser
+          user={user}
+          users={users}
+          setUsers={setUsers}
+          toggleEdit={toggleEdit}
+        />
+      ) : (
+        <div>
+          <div>
+            <h2>{user.name}</h2>
+            <p>{user.bio}</p>
+          </div>
+          <div>
+            <button onClick={toggleEdit}>Edit</button>
+            <button onClick={deleteUser}>Delete</button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
